88362e436b5d5af19b3d990733904f7e5f03834e
Nawal Hafez <nawal.hafez@egym.com>
2025-10-30 17:16:42 +0100

    be able to stop main

--- bodytracking/bodytracking/main.py
+++ bodytracking/bodytracking/main.py
@@ -5,13 +5,17 @@ import struct
 import global_vars
 from sys import exit
 
+thread = BodyThread()
 
 try:
-    thread = BodyThread()
     thread.start()
-# make this react to keyboard interruptions
-except KeyboardInterrupt:
 
+    # Keep main thread alive to catch KeyboardInterrupt
+    while not global_vars.KILL_THREADS:
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nShutting down...")
     global_vars.KILL_THREADS = True
-    time.sleep(0.5)
-    exit()
+    thread.join(timeout=2.0)  # Wait for thread to finish
+    exit(0)
